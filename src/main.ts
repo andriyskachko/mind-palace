2caf1a5e1ecb7c423ec3337a620cc48083eeafc6
Andriy Skachko <skachko.andrii@gmail.com>
2023-03-30 23:26:59 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import './sass/main.scss';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter';
+import { html } from './utilities/html';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = html`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
